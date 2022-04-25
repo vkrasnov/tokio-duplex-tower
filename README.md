63e77869c8de03ad5c625c5d293e539bb2a942a8
Vlad Krasnov <vlad@readyset.io>
2022-04-25 14:54:56 -0400

    Add README and lib doc

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# tokio-duplex-tower
+# tower-duplex
+
+A [`tower::Service`] that implements a server and a client simultaneously over a
+bi-directional channel. As a server it is able to process RPC calls from a remote client,
+and as a client it is capable of making RPC calls into a remote server. It is very
+convinient in a system that requires asynchronous communication in both directions.
+
+License: MIT OR Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! A [`tower::Service`] that implements a server and a client simultaneously over a
+//! bi-directional channel. As a server it is able to process RPC calls from a remote client,
+//! and as a client it is capable of making RPC calls into a remote server. It is very
+//! convinient in a system that requires asynchronous communication in both directions.
 use std::collections::VecDeque;
 use std::future::Future;
 use std::marker::PhantomData;
